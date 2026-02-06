71e05f1501f18c3825972f574f553168a521ca9a
DennisGabriel-Dev <dennisgabr745@gmail.com>
2026-02-05 23:58:00 -0300

    :sparkles: refactor courses controller to streamline HTML responses, add API V1 courses controller for JSON handling, and update routes for API support

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,16 +1,13 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
 
   def index
     # Apenas cursos do tenant atual serão retornados automaticamente
     @courses = Course.all
-    respond_to do |format|
-      format.html
-      format.json { render json: @courses }
-    end
   end
 
-  def show; end
+  def show
+  end
 
   def new
     @course = Course.new
@@ -21,55 +18,26 @@ class CoursesController < ApplicationController
     @course = Course.new(course_params)
 
     if @course.save
-      respond_to do |format|
-        format.html { redirect_to @course, notice: "Curso #{@course.name} criado com sucesso." }
-        format.json { render json: {
-            course: @course,
-            message: "Curso #{@course.name} criado com sucesso."
-        }, status: :created }
-      end
+      redirect_to @course, notice: "Curso criado com sucesso."
     else
-      respond_to do |format|
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
-      end
+      render :new, status: :unprocessable_entity
     end
   end
 
-  def edit; end
+  def edit
+  end
 
   def update
     if @course.update(course_params)
-      respond_to do |format|
-        format.html { redirect_to @course, notice: "Curso #{@courser.name} atualizado com sucesso." }
-        format.json { render json: {
-            course: @course,
-            message: "Curso #{@course.name} atualizado com sucesso."
-        }, status: :ok }
-      end
+      redirect_to @course, notice: "Curso atualizado com sucesso."
     else
-      respond_to do |format|
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
-      end
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    if @course.destroy
-      respond_to do |format|
-        format.html { redirect_to courses_path, notice: "Curso #{@course.name} deletado com sucesso." }
-        format.json { render json: {
-            course: @course,
-            message: "Curso #{@course.name} deletado com sucesso."
-        }, status: :ok }
-      end
-    else
-      respond_to do |format|
-        format.html { redirect_to courses_path, alert: "Erro ao deletar curso #{@course.name}." }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
-      end
-    end
+    @course.destroy
+    redirect_to courses_path, notice: "Curso removido com sucesso."
   end
 
   private
